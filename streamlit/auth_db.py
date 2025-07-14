22424558ccc096ba847245b57ab9e5fbaae0d532
Yaparla Bhargavi <yaparlabhargavi023@gmail.com>
2025-07-14 19:17:57 +0530

    Update auth_db.py

--- streamlit/auth_db.py
+++ streamlit/auth_db.py
@@ -10,3 +10,15 @@ conn = mysql.connector.connect(
 
 # Create a cursor object
 csr = conn.cursor()
+import streamlit as st
+import mysql.connector
+
+secrets = st.secrets["mysql"]
+conn = mysql.connector.connect(
+    host=secrets["localhost"],
+    user=secrets["root"],
+    password=secrets["cseds@32"],
+    database=secrets["project2"]
+)
+
+csr = conn.cursor()
