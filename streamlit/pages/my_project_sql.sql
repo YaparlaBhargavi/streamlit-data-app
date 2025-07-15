547c70728fe11dbbecbac6534cf0688d8270946c
Yaparla Bhargavi <yaparlabhargavi023@gmail.com>
2025-07-15 18:53:55 +0530

    Update my_project_sql.sql

--- streamlit/pages/my_project_sql.sql
+++ streamlit/pages/my_project_sql.sql
@@ -1,11 +1,11 @@
 -- 1. Create a new database (if not exists)
-CREATE DATABASE project2;
+CREATE DATABASE railway;
 
 -- 2. Switch to that database
-USE project2;
+USE railway;
 
 -- 3. Create the signup_user table
-CREATE TABLE  signup_use (
+CREATE TABLE  signup_us (
     username VARCHAR(120) UNIQUE,
     Full_name VARCHAR(255),
     phone VARCHAR(12),
@@ -14,8 +14,8 @@ CREATE TABLE  signup_use (
 );
 
 -- 4. Insert data into the table (replace values with real data)
-INSERT INTO signup_use (username, Full_name, phone, email, passwordd)
+INSERT INTO signup_us (username, Full_name, phone, email, passwordd)
 VALUES ('your_username', 'Your Full Name', '9876543210', 'your@email.com', 'your_password');
 
 -- 5. (Optional) View all data in the table
-SELECT * FROM signup_use;
+SELECT * FROM signup_us;
